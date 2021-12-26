fde7b23d296d81e20d141008fdf54ffb8beb4fab
dogeplusplus <albertswchung@gmail.com>
2021-12-26 16:26:03 +0000

    Add dataset preprocessing functionality for label-studio + reddit scraping outputs.

--- load/dataset.py
+++ load/dataset.py
@@ -1,23 +1,29 @@
-import glob
 import librosa
-import torchaudio
+import logging
 import pandas as pd
 
-from librosa.feature import mfcc
+from pathlib import Path
+from librosa.feature import melspectrogram
 from torch.utils.data import Dataset
+
+
+logger = logging.getLogger(__name__)
 
 
 class MeowDataset(Dataset):
     def __init__(self, audio_directory):
         self.audio_directory = audio_directory
-        label_path = f"{self.audio_directory}/labels.csv"
+        label_path = Path(self.audio_directory, "labels.csv")
         self.labels = pd.read_csv(label_path)
-        self.audio_paths = glob.glob(f"{self.audio_directory}/*.wav")
+        self.audio_paths = list(Path(self.audio_directory).glob("**/*.wav"))
 
     def __getitem__(self, idx):
-        waveform, _ = librosa.load(self.audio_paths[idx])
-        mfcc_sequence = mfcc(waveform, n_mfcc=40)
+        path = self.audio_paths[idx]
+        waveform, sample_rate = librosa.load(path, sr=None)
+        mfcc_sequence = melspectrogram(waveform, sr=sample_rate)
+        audio_duration = waveform.size / sample_rate
         return mfcc_sequence
 
     def __len__(self):
         return len(self.audio_paths)
+
--- transform/preprocessing.py
+++ transform/preprocessing.py
@@ -1,61 +1,15 @@
-import os
+import librosa
+import logging
 import numpy as np
 import pandas as pd
 
-from tqdm import tqdm
-from pydub import AudioSegment
-from scipy.io.wavfile import write
+from ast import literal_eval
+from librosa.feature import mfcc
+from prefect import Parameter, Flow, task
+from prefect.executors import LocalDaskExecutor
 
 
-SAMPLE_RATE = 16384
-
-
-def preprocess_dataset(sound_files, destination_folder, format="mp3"):
-    """
-    Standardise audio for training
-
-    Preprocess audio files to 1 second wav files resampled at 16k.
-
-    Parameters
-    ----------
-    sound_files : List[str]
-        List of sound file paths
-    destination_folder : str
-        Desination folder where to save post-processed audio
-    format : str
-        Type of the input audio (mp3, wav)
-    """
-    os.makedirs(destination_folder, exist_ok=True)
-
-    for i, path in enumerate(tqdm(sound_files)):
-        sound = AudioSegment.from_file(path, format=format)
-        resampled = sound.set_frame_rate(SAMPLE_RATE)
-        waveform = np.array(resampled.get_array_of_samples())
-        second = peak_second(waveform)
-        write(f"{destination_folder}/{i}.wav", SAMPLE_RATE, second)
-
-
-def peak_second(sound):
-    """
-    Heuristic to segment 1 second in the sound file.
-
-    Locate the peak signal and crop the audio to a 1 second interval.
-
-    Parameters
-    ----------
-    sound : np.array
-        Audio segment to crop
-    """
-    peak_location = np.argmax(sound)
-    offset = SAMPLE_RATE // 2
-
-    if peak_location < offset:
-        peak_location = offset
-    elif len(sound) - peak_location < offset:
-        peak_location = len(sound) - offset
-
-    return sound[peak_location-offset:peak_location+offset]
-
+logger = logging.getLogger(__name__)
 
 def rename_audio_paths(labels_path):
     """
@@ -65,11 +19,115 @@ def rename_audio_paths(labels_path):
 
     Parameters
     ----------
-    labels_path: str
+    labels_path: Path
         Path to the labels
     """
     df = pd.read_csv(labels_path)
-    dataset_directory = os.path.dirname(labels_path)
-    convert = lambda x: os.path.join(dataset_directory, x[24:])
+    dataset_directory = labels_path.parent
+    convert = lambda x: dataset_directory.join_path(x[24:])
     df["audio"] = df["audio"].apply(convert)
     df.to_csv(labels_path, index=False)
+
+
+def preprocess_labels(raw_label, audio_duration, time_frames):
+    """
+    Parse labels into an array with class numbers.
+
+    Quantize continuous audio labels into discrete time frames based on the MFCC spectrogram.
+
+    Parameters
+    ----------
+    raw_label: str
+        Raw JSON string containing labels from label-studio
+    audio_duration: float
+        Duration of the entire audio file in seconds
+    time_frames: int
+        Number of discrete frames after applying MFCC to the waveform
+
+    Returns
+    -------
+    label: np.array[int]
+        Array with marked labels for Background, Cat, Human (0, 1, 2)
+    """
+    mapping = {
+        "Cat": 1,
+        "Human": 2,
+    }
+    label = np.zeros((time_frames))
+    index = lambda x: int(time_frames * x / audio_duration)
+
+    try:
+        labelled_segments = literal_eval(raw_label)
+        for segment in labelled_segments:
+            start = index(segment["start"])
+            end = index(segment["end"])
+            label[start:end] = mapping[segment["labels"][0]]
+    except ValueError:
+        logger.info("Skipping since could not parse label, likely to be nan.")
+
+    return label
+
+@task
+def preprocess_dataset(audio_path, label_path, dest_dir, n_mfcc):
+    """
+    Preproces label studio data and wav files into arrays to save computational time.
+
+    Convert waveforms into MFCCs, JSON labels into array.
+
+    Parameters
+    ----------
+    audio_path: Path
+        Path to folder containing audio wav files
+    label_path: Path
+        Path to the labels csv from label studio
+    dest_dir: Path
+        Path to store preprocessed dataset
+    n_mfcc: int
+        Number of frequency bands for MFCC conversion
+    """
+    labels = pd.read_csv(label_path)
+
+    audio_dir = dest_dir.joinpath("audio")
+    label_dir = dest_dir.joinpath("labels")
+
+    dest_dir.mkdir(parents=True, exist_ok=True)
+    audio_dir.mkdir(exist_ok=True)
+    label_dir.mkdir(exist_ok=True)
+
+
+    audio_files = list(audio_path.glob("**/*.wav"))
+    for file_name in audio_files:
+        waveform, sample_rate = librosa.load(file_name, sr=None)
+        mel_coefficients = mfcc(waveform, sample_rate, n_mfcc=n_mfcc)
+
+        num_frames = mel_coefficients.shape[-1]
+        duration = waveform.shape[-1] / sample_rate
+        raw_label = labels.loc[labels["audio"] == str(file_name), "label"].values[0]
+        label = preprocess_labels(raw_label, duration, num_frames)
+
+        base_name = file_name.stem
+        np.save(audio_dir.joinpath(f"{base_name}.npy"), mel_coefficients)
+        np.save(label_dir.joinpath(f"{base_name}.npy"), label)
+
+
+def build_flow():
+    with Flow("Dataset Numpy Conversion") as flow:
+        audio_path = Parameter("audio_path")
+        label_path = Parameter("label_path")
+        dest_dir = Parameter("dest_dir")
+        n_mfcc = Parameter("n_mfcc")
+
+        preprocess_dataset(audio_path, label_path, dest_dir, n_mfcc)
+
+    return flow
+
+
+def main():
+    # TODO: Either add argunment passing or a better way of organising/running flows
+    flow = build_flow()
+    flow.executor = LocalDaskExecutor(scheduler="processes")
+    flow.run()
+
+
+if __name__ == "__main__":
+    main()
